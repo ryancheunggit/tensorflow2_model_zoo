1f68ede1204ccfb88a6e6d4f9ae33ee48f7f9506
renzhang <renzhang@nielsen.com>
2019-12-18 13:37:04 -0500

    refactor the smooth l1 loss

--- losses/regression.py
+++ losses/regression.py
@@ -1,17 +1,31 @@
 import tensorflow as tf
 
 
-def smooth_l1_loss(labels, predictions):
+def smooth_l1_loss(y_true, y_pred):
+    y_pred = tf.convert_to_tensor(y_pred)
+    y_true = tf.cast(y_true, y_pred.dtype)
+    error = y_true - y_pred
+    abs_error = tf.abs(error)
+    mask = abs_error < 1
+    squared_loss = tf.square(error)
+    linear_loss = abs_error - .5
+    loss = tf.where(mask, squared_loss, linear_loss)
+    return tf.reduce_mean(loss, 1)
+
+
+class SmoothL1Loss(tf.keras.losses.Loss):
     """Smooth L1 Loss
 
-    Use squared loss when difference is wihtin 1, otherwise use an L1 loss
+    Use squared loss when difference is wihtin 1, otherwise use an L1 loss.
+    It turned out is is just [Huber loss](https://en.wikipedia.org/wiki/Huber_loss) with delta = 1.
+    Anyway, implmented as an exercise.
 
     reference: http://arxiv.org/abs/1504.08083
     """
-    n = tf.cast(tf.reduce_prod(tf.shape(labels)), dtype=tf.float32)
-    l1_mask = tf.abs(labels - predictions) >= 1
-    l2_mask = tf.abs(labels - predictions) < 1
-    z1 = tf.abs(tf.boolean_mask(labels, l1_mask) - tf.boolean_mask(predictions, l1_mask)) - .5
-    z2 = .5 * tf.square(tf.boolean_mask(labels, l2_mask) - tf.boolean_mask(predictions, l2_mask))
-    loss = (tf.reduce_sum(z1) + tf.reduce_sum(z2)) / n
-    return loss
+    def __init__(self,
+                 reduction=tf.keras.losses.Reduction.NONE,
+                 name='smooth_l1_loss'):
+        super(SmoothL1Loss, self).__init__(name=name, reduction=reduction)
+
+    def call(self, y_true, y_pred):
+       return smooth_l1_loss(y_true, y_pred)
